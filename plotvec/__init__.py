f7399e481aef5b26dbe2ddcf5f1aeed1dc2712dc
John M. Shea <jshea@ece.ufl.edu>
2023-12-19 21:53:52 -0500

    Add plot_field function that shows effect of a linear transformation on a square field of points

--- plotvec/__init__.py
+++ plotvec/__init__.py
@@ -10,5 +10,7 @@ for the book Introduction to Data Science for Engineers
 All files in the package are distributed under the MIT License
 '''
 
-__version__ = '1.5.1'
+__version__ = '1.6.0'
+
 from .plotvec import plotvec, plotvecR
+from .transforms import plot_field
---
+++ plotvec/transforms.py
@@ -0,0 +1,52 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+def plot_field (matrix=np.eye(2), field_width=3, point_spacing=0.5,
+                preserve_axes=True, colormap='plasma'):
+
+  ''' Plot the outputs of a linear transform on a square field of points
+
+  Generates a uniformly spaced field of points in a 2-D square,
+  applies the linear transform to the points, and plots the
+  field of output points
+
+  Parameters
+  ----------
+  matrix : ndarray
+    2D matrix that specifies the linear transform
+
+  field_width: number
+    size of input point field in each dimension
+
+  point_spacing: number
+    spacing between field points in each dimension
+
+  preserve_axes: boolean
+    keep limits of output axes same as input space
+
+  colormap: string
+    Matplotlib colormap
+  '''
+
+
+  cmap = mpl.colormaps[colormap]
+
+  xs = np.arange(-field_width, field_width + point_spacing,
+                 point_spacing)
+  ys = xs
+
+  for x in xs:
+    for y in ys:
+      angle = np.arctan2(y, x) 
+      if angle < 0:
+        angle += 2*np.pi
+      rgba = cmap(angle / (2 * np.pi)   ) 
+      out = matrix @ np.array([x, y])
+      plt.scatter(out[0], out[1], 2, color=rgba)
+
+  ax = plt.gca()
+  ax.axis('equal')
+  if preserve_axes:
+    plt.xlim(-field_width, field_width)
+    plt.ylim(-field_width, field_width)
